9a732e8f61a5f547451c94f2f7b757c79037d099
lucas-almeida12 <lucas1998pc@gmail.com>
2025-07-25 09:42:51 -0300

    Restaurado.js

--- api/audio.js
+++ api/audio.js
@@ -8,25 +8,15 @@ module.exports = async (req, res) => {
   }
   const url = `https://drive.google.com/uc?export=download&id=${id}`;
   try {
-    // Repasse o header Range se existir
-    const headers = {};
-    if (req.headers.range) {
-      headers['Range'] = req.headers.range;
-    }
+    const response = await fetch(url);
 
-    const response = await fetch(url, { headers });
-
-    if (!response.ok && response.status !== 206) {
+    if (!response.ok) {
       return res.status(response.status).send('Erro ao acessar o Google Drive');
     }
 
-    // Repasse os headers relevantes
-    for (const [key, value] of response.headers) {
-      res.setHeader(key, value);
-    }
+    // Permitir CORS para qualquer origem
     res.setHeader('Access-Control-Allow-Origin', '*');
-
-    res.status(response.status);
+    res.setHeader('Content-Type', 'audio/mpeg');
     response.body.pipe(res);
   } catch (err) {
     res.status(500).send('Erro no proxy: ' + err.message);
