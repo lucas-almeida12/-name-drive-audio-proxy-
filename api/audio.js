1ec92935f9d107f483d8a72f9244916d1ad0796b
lucas-almeida12 <lucas1998pc@gmail.com>
2025-07-25 08:59:47 -0300

    Update audio.js

--- api/audio.js
+++ api/audio.js
@@ -8,15 +8,25 @@ module.exports = async (req, res) => {
   }
   const url = `https://drive.google.com/uc?export=download&id=${id}`;
   try {
-    const response = await fetch(url);
+    // Repasse o header Range se existir
+    const headers = {};
+    if (req.headers.range) {
+      headers['Range'] = req.headers.range;
+    }
 
-    if (!response.ok) {
+    const response = await fetch(url, { headers });
+
+    if (!response.ok && response.status !== 206) {
       return res.status(response.status).send('Erro ao acessar o Google Drive');
     }
 
-    // Permitir CORS para qualquer origem
+    // Repasse os headers relevantes
+    for (const [key, value] of response.headers) {
+      res.setHeader(key, value);
+    }
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Content-Type', 'audio/mpeg');
+
+    res.status(response.status);
     response.body.pipe(res);
   } catch (err) {
     res.status(500).send('Erro no proxy: ' + err.message);
